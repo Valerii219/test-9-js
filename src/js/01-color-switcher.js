774675d6f90fa00f942c04920aad49f2de4fe308
Valerii Arlamovskyi <valera.arl96@gmail.com>
2024-04-14 15:26:12 -0400

    add color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,11 +1,46 @@
 const refs = {
-start:document.querySelector('button[data-start]'),
-stop:document.querySelector('button[data-stop]')
+  body: document.querySelector('body'),
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
+let time = null;
+const delay = 1000;
+
+refs.start.addEventListener('click', changecolor);
+refs.stop.addEventListener('click', stopChangeColor);
+
+disabledButtonStop();
+
+function disabledButtonStart() {
+  refs.start.setAttribute('disabled', 'true');
+}
+function ableButtonStart() {
+  refs.start.removeAttribute('disabled');
 }
 
+function disabledButtonStop() {
+  refs.stop.setAttribute('disabled', 'true');
+}
+function ableButtonStop() {
+  refs.stop.removeAttribute('disabled');
+}
 
-function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  }
+function changecolor() {
+  ableButtonStop();
+  disabledButtonStart();
+  time = setInterval(() => {
+    refs.body.style.backgroundColor = randomColor();
+  }, delay);
+}
 
+function stopChangeColor() {
+  ableButtonStart();
+  clearInterval(time);
+  disabledButtonStop();
+}
 
+const randomColor = function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
