700186989de3d6f6d545a29d8f6a1a4eef9e5529
Valerii Arlamovskyi <valera.arl96@gmail.com>
2024-04-25 19:36:58 -0400

    f

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,19 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve > 0.3) {
+        resolve('Success! Value passed to resolve function');
+      } else {
+        reject('Error! Error passed to reject function');
+      }
+    }, 2000);
+  });
 }
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
